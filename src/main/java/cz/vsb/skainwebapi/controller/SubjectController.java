8349223b498476e2a8f69aeb4e0f32579e994f61
Stepan <110170403+skain1918@users.noreply.github.com>
2025-06-17 13:52:03 +0200

    subjectController works correctly

--- src/main/java/cz/vsb/skainwebapi/controller/SubjectController.java
+++ src/main/java/cz/vsb/skainwebapi/controller/SubjectController.java
@@ -3,9 +3,7 @@ package cz.vsb.skainwebapi.controller;
 import cz.vsb.skainwebapi.dto.SubjectDto;
 import cz.vsb.skainwebapi.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,17 @@ public class SubjectController {
     @GetMapping({"/subjects", "/subjects/"})
     public List<SubjectDto> getSubjects(){
         return subjectService.getSubjects();
+    }
+    @PostMapping({"/subjects", "/subjects/"})
+    public SubjectDto addSubject(@RequestBody SubjectDto subjectToAdd){
+        return subjectService.addSubject(subjectToAdd);
+    }
+    @PutMapping("subjects/{subjectId}")
+    public SubjectDto editSubject(@RequestBody SubjectDto editedSubject, @PathVariable int subjectId){
+        return subjectService.editSubject(editedSubject, subjectId);
+    }
+    @DeleteMapping("subject/{subjectId}")
+        public SubjectDto deleteSubject(@PathVariable int subjectId){
+        return subjectService.deleteSubject(subjectId);
     }
 }
--- src/main/java/cz/vsb/skainwebapi/service/SubjectService.java
+++ src/main/java/cz/vsb/skainwebapi/service/SubjectService.java
@@ -2,10 +2,9 @@ package cz.vsb.skainwebapi.service;
 
 import cz.vsb.skainwebapi.dto.SubjectDto;
 import cz.vsb.skainwebapi.entity.SubjectEntity;
-import cz.vsb.skainwebapi.mapper.StudentMapper;
 import cz.vsb.skainwebapi.mapper.SubjectMapper;
-import cz.vsb.skainwebapi.repository.StudentRepository;
 import cz.vsb.skainwebapi.repository.SubjectRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +18,12 @@ public class SubjectService {
 
     @Autowired
     public SubjectService(SubjectMapper subjectMapper, SubjectRepository subjectRepository) {
-        subjectMapper = subjectMapper;
+        this.subjectMapper = subjectMapper;
         this.subjectRepository = subjectRepository;
     }
 
 
-    public List<SubjectDto> getSubjects(){
+    public List<SubjectDto> getSubjects() {
         List<SubjectEntity> subjectEntities = subjectRepository.findAll();
         List<SubjectDto> subjectDtos = new ArrayList<>();
         for (SubjectEntity subjectEntity : subjectEntities) {
@@ -33,5 +32,28 @@ public class SubjectService {
         return subjectDtos;
     }
 
+    public SubjectDto addSubject(SubjectDto newSubjectDto) {
+        SubjectEntity subjectEntity = subjectMapper.toEntity(newSubjectDto);
+        SubjectEntity savedSubject = subjectRepository.save(subjectEntity);
+        return subjectMapper.toDto(savedSubject);
+    }
 
+    public SubjectDto editSubject(SubjectDto editedSubject, int subjectId) {
+        if (!subjectRepository.existsById(subjectId)) {
+            throw new EntityNotFoundException("Subject not found");
+        }
+        SubjectEntity entity = subjectMapper.toEntity(editedSubject);
+        entity.setId(subjectId);
+        SubjectEntity savedSubject = subjectRepository.save(entity);
+        return subjectMapper.toDto(savedSubject);
+    }
+    public SubjectDto deleteSubject(int subjectId){
+        if (!subjectRepository.existsById(subjectId)) {
+            throw new EntityNotFoundException("Subject not found");
+        }
+        SubjectEntity entity = subjectRepository.findById(subjectId).orElseThrow(EntityNotFoundException::new);
+        SubjectDto deletedSubjectToReturn = subjectMapper.toDto(entity);
+        subjectRepository.delete(entity);
+        return deletedSubjectToReturn;
+    }
 }
